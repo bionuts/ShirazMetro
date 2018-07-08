6de257d649d525abdc1f2672df432bab917f2bbe
Reza Jahan <mreza.jahankhah@gmail.com>
2018-07-08 07:55:38 +0430

    radius cardview

--- app/src/main/java/ir/shirazmetro/shirazmetro/TilesActivity.java
+++ app/src/main/java/ir/shirazmetro/shirazmetro/TilesActivity.java
@@ -2,9 +2,6 @@ package ir.shirazmetro.shirazmetro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-
-import ir.shirazmetro.shirazmetro.MyImageHelper;
 
 public class TilesActivity extends AppCompatActivity {
 
@@ -13,7 +10,5 @@ public class TilesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiles);
 
-        ImageView iv = (ImageView)findViewById(R.id.imageVdieddfdffdw4);
-        // MyImageHelper.getRoundedCornerBitmap((iv.bit))
     }
 }
