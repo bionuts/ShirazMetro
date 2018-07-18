c08268442860e5c8699b93d601501b98e9244b03
Reza Jahan <mreza.jahankhah@gmail.com>
2018-07-18 09:56:33 +0430

    fixing video player

--- app/src/main/java/ir/shirazmetro/shirazmetro/TilesActivity.java
+++ app/src/main/java/ir/shirazmetro/shirazmetro/TilesActivity.java
@@ -1,14 +1,21 @@
 package ir.shirazmetro.shirazmetro;
 
+import android.net.Uri;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.URI;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.ArrayList;
 
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import ir.shirazmetro.shirazmetro.applib.MySlideAdapter;
 import me.relex.circleindicator.CircleIndicator;
@@ -16,6 +23,11 @@ import me.relex.circleindicator.CircleIndicator;
 public class TilesActivity extends AppCompatActivity {
 
     private ViewPager mPager;
+    VideoView video;
+    private MediaController mc;
+
+    Button btn_play;
+    String videoPath;
     private static int currentPage = 0;
     private static final Integer[] XMEN = {R.drawable.train1, R.drawable.train2, R.drawable.train3, R.drawable.train4, R.drawable.train5};
     private ArrayList<Integer> XMENArray = new ArrayList<>();
@@ -26,6 +38,21 @@ public class TilesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiles);
         // init();
+        if (mc == null) {
+            mc = new MediaController(this);
+        }
+        video = (VideoView) findViewById(R.id.videoView);
+        video.setMediaController(mc);
+        btn_play = (Button) findViewById(R.id.button3);
+    }
+
+    public void PlayVideoOnClick(View v) {
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT);
+        videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Toast.makeText(this, videoPath, Toast.LENGTH_LONG);
+        Uri videoUri = Uri.parse(videoPath);
+        video.setVideoURI(videoUri);
+        video.start();
     }
 
     /*private void init() {
