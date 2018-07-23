83a80719b8532c5570404ca1fb9a8738156b23f6
Reza Jahan <mreza.jahankhah@gmail.com>
2018-07-23 12:04:19 +0430

    final main layout11

--- app/src/main/java/ir/shirazmetro/shirazmetro/TilesActivity.java
+++ app/src/main/java/ir/shirazmetro/shirazmetro/TilesActivity.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.ScrollView;
@@ -41,6 +42,7 @@ public class TilesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_tiles);
         init();
         if (mc == null) {
