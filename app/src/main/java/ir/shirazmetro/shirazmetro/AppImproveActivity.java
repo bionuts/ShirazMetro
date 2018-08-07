1f11eff507c3094c6eae91639603000322f338e2
Reza Jahan <mreza.jahankhah@gmail.com>
2018-08-07 11:03:27 +0430

    contact us layout7

---
+++ app/src/main/java/ir/shirazmetro/shirazmetro/AppImproveActivity.java
@@ -0,0 +1,13 @@
+package ir.shirazmetro.shirazmetro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppImproveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_improve);
+    }
+}
--- app/src/main/java/ir/shirazmetro/shirazmetro/MainActivity.java
+++ app/src/main/java/ir/shirazmetro/shirazmetro/MainActivity.java
@@ -87,6 +87,11 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 
+    public void onButton7Clicked(View view) {
+        Intent intent = new Intent(getApplicationContext(), AppImproveActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
